f108d203421221b5c76e39f4204a0f400e3e35b6
Katherine Sullivan <katherineS23@lakesideschool.org>
2021-02-03 21:46:10 -0800

    Basic File Structure

---
+++ app.py
@@ -0,0 +1,31 @@
+#configuring Flask
+import flask
+from flask_mysqldb import MySQL
+from flask_htpasswd import HtPasswdAuth
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+
+@app.route('/', methods=["GET", "POST"])
+
+#function that renders index.html
+def index(methods=["GET"]):
+    return render_template('index.html');
+
+@app.route('/us', methods=["GET", "POST"])
+
+#function that renders speakers.html
+def us(methods=["GET"]):
+        return render_template('us.html')
+
+@app.route('/world', methods=["GET", "POST"])
+#function that renders secretSpeakers.html
+def secretSpeakers():
+    return render_template('world.html');
+
+
+@app.route('/business', methods=["GET", "POST"])
+
+#function that renders contact.html
+def contact(methods=["POST"]):
+    return render_template('business.html')
